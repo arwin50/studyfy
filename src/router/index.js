381aa1a1445ac14894dba6e64ad83e14cc37f1f0
powtzulist <delasanarwin@gmail.com>
2024-03-24 19:04:56 +0800

    frontend route protection

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import AboutView from '@/views/AboutView.vue'
 import HomeView from '../views/HomeView.vue'
 import PostView from '@/views/PostView.vue'
 import QuestionView from '@/views/QuestionView.vue'
+import axios from 'axios'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,8 @@ const router = createRouter({
     {
       path: '/newPost',
       name: 'newPost',
-      component: PostView
+      component: PostView,
+      meta: { requiresAuth: true }
     },
     {
       path: '/question/:questionId',
@@ -28,6 +30,28 @@ const router = createRouter({
       component: QuestionView
     }
   ]
+})
+
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requiresAuth) {
+    let user = ''
+    try {
+      const response = await axios.get('http://localhost:5000/usermounted', {
+        withCredentials: true
+      })
+      user = response.data
+    } catch (error) {
+      console.error(error)
+    }
+
+    if (user._id) {
+      next()
+    } else {
+      next({ name: 'home' })
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
