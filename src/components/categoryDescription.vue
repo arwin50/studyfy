3f7df27296573605c95379ffff1cd9c55a8718af
powtzulist <delasanarwin@gmail.com>
2024-03-26 22:57:14 +0800

    initialized category description and fixed google image bug

--- src/components/categoryDescription.vue
+++ src/components/categoryDescription.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router'
+import { ref, watch } from 'vue'
+
+const route = useRoute()
+let category = ref(route.params.category)
+
+watch(
+  () => route.params.category,
+  (newValue) => {
+    category.value = newValue
+  }
+)
+</script>
 
 <template>
-  <div></div>
+  <div class="w-full bg-white py-8 px-10 border-b">
+    <span class="text-4xl font-semibold">{{ category }}/questions</span>
+  </div>
 </template>
